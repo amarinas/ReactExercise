8303a3d88bee2f37753a0929651e40ed2fc33e26
ali <ali.marinas@icloud.com>
2019-11-30 22:03:29 -0800

    change to class instead of function

--- practice/App.js
+++ practice/App.js
@@ -2,15 +2,21 @@ import React from "react"
 import ReactDom from "react-dom"
 import TodoItem from "./components/TodoItem"
 
-function App() {
-    return (
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      todoItems: todoData.map(item => </TodoItem key={item.id} item={item} />)
+
+      }
+    }
+    render(){
+      return(
         <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {this.todoItems}
         </div>
-    )
+      )
+  }
 }
 
 export default App
